4699c31f48233574768daaf31bf0fd5e3f91ddf7
Surya <SuryaGopisetty30@gmail.com>
2023-09-23 21:52:08 +0530

    Added Is Valid Code

--- blockchain.py
+++ blockchain.py
@@ -88,6 +88,17 @@ def get_chain():
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
+# Is Valid
+
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'We have a problem. The Blockchain is not valid.'}
+    return jsonify(response), 200
+
 # Running the app
 
 app.run(host = '0.0.0.0', port = '5000')
